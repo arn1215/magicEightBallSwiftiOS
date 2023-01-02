1b76cf59d8469a78e9b28130191064456659659b
ali <ali.naqvi.dev@gmail.com>
2023-01-01 18:39:37 -0600

    removed 8 ball on initial load

--- ViewController.swift
+++ ViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var eightBallImg: UIImageView!
     
     let ballArray = [#imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball1")]
-
+ 
     @IBAction func onAsk(_ sender: UIButton) {
+        
         eightBallImg.image = ballArray.randomElement()
     }
     
