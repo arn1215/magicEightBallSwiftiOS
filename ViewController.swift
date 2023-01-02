96c8c80e81a37f94aaf0ba8e5f7ba33ab2d107bb
ali <ali.naqvi.dev@gmail.com>
2023-01-01 18:22:28 -0600

    randomized eightball response

--- ViewController.swift
+++ ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var eightBallImg: UIImageView!
+    
+    let ballArray = [#imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball1")]
 
-
-
+    @IBAction func onAsk(_ sender: UIButton) {
+        eightBallImg.image = ballArray.randomElement()
+    }
+    
 }
 
